bb7b19eaa7d9a873884773f39f6cfe5bd1fc032f
HAONANTAO <873190199@qq.com>
2024-04-06 23:17:37 +1100

    dotenv get dataurl from env file

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,14 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 // 创建服务器express
 const app = express();
 
+// 安全问题 从env拿到数据库连接和密码
+// Load environment variables from .env file
+dotenv.config();
+const DATABASE_URL = process.env.DATABASE_URL;
+mongoose.connect(DATABASE_URL).then(console.log("connected"));
 app.listen(3000, () => {
   console.log("listening");
 });
