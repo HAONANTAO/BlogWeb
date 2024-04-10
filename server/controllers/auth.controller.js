1f22a0ac0c5040949c253af0bb061cdd91ab5f85
HAONANTAO <873190199@qq.com>
2024-04-10 15:55:13 +1000

    signin api works

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
@@ -29,6 +30,45 @@ export const signup = async (req, res, next) => {
     return res.status(200).json({ message: "Signup successfully!" });
   } catch (error) {
     //pass error to next
+    return next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { password, email } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required!"));
+  }
+  try {
+    const validUser = await User.findOne({
+      // use email and password login
+      email,
+    });
+    if (!validUser) {
+      next(errorHandler(404, "user can not found!"));
+    }
+    //compare password
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(404, "invalid password!"));
+    }
+    //all passed
+    //JWT Token
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET_KEY,
+      // { expiresIn: "1d" },
+    );
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        //for security reason
+        httpOnly: true,
+      })
+      .json(validUser);
+  } catch (error) {
     return next(error);
   }
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { signup } from "../controllers/auth.controller.js";
+import { signin } from "../controllers/auth.controller.js";
 const authRoutes = express.Router();
 
 authRoutes.post("/signup", signup);
+authRoutes.post("/signin", signin);
 
 export default authRoutes;
