0564d63692c45de4e7f85e771ef094423dbcb0ef
HAONANTAO <873190199@qq.com>
2024-04-17 16:57:29 +1000

    date update

--- client/src/components/Dashboard/DashPosts.jsx
+++ client/src/components/Dashboard/DashPosts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
@@ -11,14 +12,27 @@ const DashPosts = () => {
           `/api/post/getposts?userId=${currentUser.data._id}`,
         );
         if (data.statusText === "OK") {
-          setUserPosts(data.data);
+          setUserPosts(data.data.posts);
         }
-        console.log(data);
       } catch (error) {}
     };
-    fetchPosts();
-  }, []);
-  return <div>sss</div>;
+    if (currentUser.data.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser.data._id]);
+  return (
+    <div>
+      {currentUser.data.isAdmin && userPosts.length > 0 ? (
+        <Table hoverable className="shadow-md">
+          <Table.Head>
+            <Table.HeadCell>Date Updated</Table.HeadCell>
+          </Table.Head>
+        </Table>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+    </div>
+  );
 };
 
 export default DashPosts;
--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -32,8 +32,9 @@ export const getPosts = async (req, res, next) => {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
+
     const posts = await Post.find({
-      ...(req.query.userId && { userId: req.query.useId }),
+      ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
       ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
