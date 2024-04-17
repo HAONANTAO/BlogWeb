0855ca83e44506787b71b973475691c20c712145
HAONANTAO <873190199@qq.com>
2024-04-17 16:29:31 +1000

    getallpost works frontend

--- client/src/components/Dashboard/DashPosts.jsx
+++ client/src/components/Dashboard/DashPosts.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 const DashPosts = () => {
-  return (
-    <div>
-      sss
-    </div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const data = await axios.get(
+          `/api/post/getposts?userId=${currentUser.data._id}`,
+        );
+        if (data.statusText === "OK") {
+          setUserPosts(data.data);
+        }
+        console.log(data);
+      } catch (error) {}
+    };
+    fetchPosts();
+  }, []);
+  return <div>sss</div>;
+};
 
-export default DashPosts
+export default DashPosts;
