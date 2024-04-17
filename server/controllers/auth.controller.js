1f2ff1a3a061bcb14fe64b5d44fb905f68f6c763
HAONANTAO <873190199@qq.com>
2024-04-17 16:16:45 +1000

    getPost api works

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -121,3 +121,4 @@ export const google = async (req, res, next) => {
     return token;
   }
 };
+
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
+
 const postRoutes = express.Router();
 
 postRoutes.post("/create", verifyUser, create);
-
+//everyone can access
+postRoutes.get('/getposts',getPosts)
 export default postRoutes;
