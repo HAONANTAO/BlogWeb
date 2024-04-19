a0b448c2f77a4eaa62a920d7ed3c39e414694802
HAONANTAO <873190199@qq.com>
2024-04-19 13:43:21 +1000

    ScrollToTop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,10 +15,12 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop"
 function App() {
   return (
     <>
       <BrowserRouter>
+      <ScrollToTop/>
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const CommentSection = ({ postId }) => {
   const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
   return (
-    <div>
-      {currentUser ? <div></div> : <div></div>}
+    <div className="w-full max-w-3xl p-3 mx-auto">
+      {currentUser ? (
+        <div className="flex items-center gap-1 my-5 text-xs text-gray-500 ">
+          <p>Signed in as: </p>
+          <img className="object-cover w-5 h-5 rounded-full" src={currentUser.data.photoURL} alt="currentUser avatar" />
+          <Link to="/dashboard?tab=profile" className="text-xs text-cyan-600 hover:underline">@{currentUser.data.username}</Link>
+        </div>
+      ) : (
+        <div className="">You must be signed in first to comment
+        <Link to="sign-in">Sign in </Link></div>
+      )}
       CommentSectionCommentSection
     </div>
   );
