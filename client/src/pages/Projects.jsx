5834fc036d09a77fbb63d46844b95a9581d1bd13
HAONANTAO <873190199@qq.com>
2024-04-20 19:52:37 +1000

    finish the rest about and projects pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
+import CallToAction from "../components/CallToAction";
 const Projects = () => {
-  return <div>Projects</div>;
-}
+  return (
+    <div className="flex flex-col items-center justify-center max-w-2xl min-h-screen gap-6 p-3 mx-auto">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-gray-500 text-md">xxxxxxxxxxxxxx</p>
+      <CallToAction />
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
