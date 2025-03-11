ad62d5e3f93ebd75d3e56000bdda2df8c96f961e
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-03-11 22:39:21 +1100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2024-04-06 20:14:19
  * @LastEditors: 陶浩南 14639548+haonantao-aaron@user.noreply.gitee.com
- * @LastEditTime: 2025-03-11 22:35:54
+ * @LastEditTime: 2025-03-11 22:39:16
  * @FilePath: /BlogWeb/client/vite.config.js
  */
 import { defineConfig } from "vite";
@@ -13,7 +13,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target:
-          "https://blog-web-eight-zeta.vercel.app/" || "http://localhost:3000", // 使用环境变量控制 API URL
+          "https://blog-web-eight-zeta.vercel.app/api" ||
+          "http://localhost:3000", // 使用环境变量控制 API URL
         secure: false, // 不使用 https 时设置为 false
         changeOrigin: true, // 确保正确代理请求
       },
